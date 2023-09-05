fbeb6752715073001c2a36dfd1f1cc1255f626d0
Piotr Mroczkowski <p.mrocz@vp.pl>
2023-09-05 19:52:30 +0200

    Add PermissionPolicyProvider

---
+++ AngularRoleAuth_Backend/AngularRoleAuth_Backend/Auth/PermissionPolicyProvider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+
+namespace AngularRoleAuth_Backend.Auth;
+
+#pragma warning disable CS8613, CS8619
+internal class PermissionPolicyProvider : IAuthorizationPolicyProvider
+{
+    public DefaultAuthorizationPolicyProvider FallbackPolicyProvider { get; }
+
+    public PermissionPolicyProvider(IOptions<AuthorizationOptions> options)
+    {
+        FallbackPolicyProvider = new DefaultAuthorizationPolicyProvider(options);
+    }
+
+    public Task<AuthorizationPolicy> GetDefaultPolicyAsync() => FallbackPolicyProvider.GetDefaultPolicyAsync();
+
+    public Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
+    {
+        if (policyName.StartsWith("Permission", StringComparison.OrdinalIgnoreCase))
+        {
+            var policy = new AuthorizationPolicyBuilder();
+            policy.AddRequirements(new PermissionRequirement(policyName));
+            return Task.FromResult(policy.Build());
+        }
+
+        return FallbackPolicyProvider.GetPolicyAsync(policyName);
+    }
+
+    public Task<AuthorizationPolicy> GetFallbackPolicyAsync() => FallbackPolicyProvider.GetDefaultPolicyAsync();
+}
