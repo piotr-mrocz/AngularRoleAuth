2cddb278ad45350250839dada13a4f96eea031a9
Piotr Mroczkowski <p.mrocz@vp.pl>
2023-09-05 19:50:30 +0200

    Add PermissionRequirement  and PermissionAuthorizationHandler

---
+++ AngularRoleAuth_Backend/AngularRoleAuth_Backend/Auth/PermissionRequirement.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace AngularRoleAuth_Backend.Auth;
+
+public class PermissionRequirement : IAuthorizationRequirement
+{
+    public string Permission { get; private set; } = null!;
+
+    public PermissionRequirement(string permission)
+    {
+        Permission = permission;
+    }
+}
+
+
+internal class PermissionAuthorizationHandler : AuthorizationHandler<PermissionRequirement>
+{
+    private IConfiguration Configuration { get; }
+
+    public PermissionAuthorizationHandler(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+#pragma warning disable CS1998
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
+    {
+        if (context.User == null)
+            return;
+
+        var permissionss = context.User.Claims
+            .Where(x =>
+                        x.Type == "Permission" &&
+                        x.Value == requirement.Permission &&
+                        x.Issuer == Configuration["JwtIssuer"]);
+
+        if (permissionss == null || !permissionss.Any())
+            return;
+
+        context.Succeed(requirement);
+        return;
+    }
+}
